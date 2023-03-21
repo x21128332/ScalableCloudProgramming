78251d375620cb5a849da10848b47162d7c1a962
Aisling Kearney <x21128332@student.ncirl.ie>
2023-03-21 18:10:44 +0000

    updating for app servcice

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):

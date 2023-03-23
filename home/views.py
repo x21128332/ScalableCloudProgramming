355f1d00436b97c203a7b31b7daa97e21ce847f0
Aisling Kearney <x21128332@student.ncirl.ie>
2023-03-23 15:26:57 +0000

    adding connection to SQL Server

--- home/views.py
+++ home/views.py
@@ -1,13 +1,33 @@
+import pyodbc
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+def get_db_connection():
+    server = 'sqlaislingsbustour.database.windows.net'
+    database = 'sqlaislingsbustour'
+    #using managed identity so no need fror uiser + pass
+    connection_string = 'Driver={ODBC Driver 18 for SQL Server};Server=tcp:sqlaislingsbustour.database.windows.net,1433;Database=sqlaislingsbustour;Authentication=ActiveDirectoryMsi; Encrypt=yes'
+    return pyodbc.connect(connection_string)
+
 # Create your views here.
 #def index(request):
-#    return HttpResponse("Hello World")
+    
+  #  return {"message": "Hello World"}
 
 
-#def index(request):
-#    return HttpResponse("Hello, world. You're at the Cats index.")
 # Create your views here.
 def index(request):
-    return render(request, "home/home.html")
+    conn = get_db_connection() 
+    cursor = conn.cursor() 
+    cursor.execute("SELECT * FROM buses") 
+    rows = cursor.fetchall()
+    # process rows    
+    conn.close() 
+   # return render(request, "home/home.html")
+    context = {"rows": rows} 
+    return render(request, "home/home.html", context)
+
+
+
+
+

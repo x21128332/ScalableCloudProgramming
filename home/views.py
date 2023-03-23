24fbc2ac6e5ff0ed051068187ab52632a11b1a75
Aisling Kearney <x21128332@student.ncirl.ie>
2023-03-23 13:39:56 +0000

    updating front end

--- home/views.py
+++ home/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
+#def index(request):
+#    return HttpResponse("Hello World")
+
+
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the Cats index.")
+# Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, "home/home.html")

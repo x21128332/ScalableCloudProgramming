4968e3a509378e947b516816a3fc965eb0d694af
Aisling Kearney <x21128332@student.ncirl.ie>
2023-03-24 14:25:52 +0000

    adding apps to settings.py

--- bustours/settings.py
+++ bustours/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'home',
+    booking,
+    routes,
+    privateHire,
+    timetables,
 ]
 
 MIDDLEWARE = [

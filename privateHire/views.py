f178515fea3149439ad2266ab8030520aa5af084
Aisling Kearney <x21128332@student.ncirl.ie>
2023-03-24 15:17:07 +0000

    fixing issue with privatehire template

--- privateHire/views.py
+++ privateHire/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def booking(request):
-    return render(request, "privateHire/privateHire.html")
+    return render(request, "privateHire/privatehire.html")

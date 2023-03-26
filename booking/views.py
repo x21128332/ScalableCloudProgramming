34bf414c607b9a969ef8fccd806f537e29a5135c
Aisling Kearney <x21128332@student.ncirl.ie>
2023-03-26 16:30:13 +0100

    testing params to booking api

--- booking/views.py
+++ booking/views.py
@@ -6,20 +6,34 @@ from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 
-def booking(request):
-    return render(request, "booking/booking.html")
+# def booking(request):
+#     return render(request, "booking/booking.html")
 
-def booking(request):
-    try:
-        # Make a GET request to a FastAPI endpoint
-        response = requests.get('https://aislingsbustours-bookingapi-staging.azurewebsites.net/bookings')
-        response.raise_for_status()
-        # Get the response data as a dictionary
-        data = response.json()
-        # Do something with the data
-        # ...
-        context = {'data': data}
-        return render(request, 'timetables/timetables.html', context)
-    except requests.exceptions.RequestException as e:
-        # handle any exceptions that occur during the request
-        return JsonResponse({'error': str(e)})
+# def booking(request):
+#     try:
+#         # Make a GET request to a FastAPI endpoint
+#         response = requests.get('https://aislingsbustours-bookingapi-staging.azurewebsites.net/bookings')
+#         response.raise_for_status()
+#         # Get the response data as a dictionary
+#         data = response.json()
+#         # Do something with the data
+#         # ...
+#         context = {'data': data}
+#         return render(request, 'booking/booking.html', context)
+#     except requests.exceptions.RequestException as e:
+#         # handle any exceptions that occur during the request
+#         return JsonResponse({'error': str(e)})
+
+    
+def booking(param1):
+    url = "https://aislingsbustours-bookingapi-staging.azurewebsites.net/bookings"
+    params = {"booking_id": param1}
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        result = response.json()["result"]
+        context = {"result": result}
+        return render(requests.request, "booking/booking.html", context)
+    else:
+        error_msg = f"Error: {response.status_code} {response.reason}"
+        context = {"error": error_msg}
+        return render(requests.request, "booking/booking.html", context)

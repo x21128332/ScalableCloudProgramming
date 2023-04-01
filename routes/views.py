600a9579aea7826b656a21f02d92264744876241
Aisling Kearney <x21128332@student.ncirl.ie>
2023-04-01 18:27:28 +0100

    test 2 on fáilte Ireland API

--- routes/views.py
+++ routes/views.py
@@ -3,24 +3,26 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
+# def routes(request):
+#     return render(request, "routes/routes.html")
+
 import http.client, urllib.request, urllib.parse, urllib.error, base64
 
-headers = {
-}
-
-params = urllib.parse.urlencode({
-})
-
-try:
-    conn = http.client.HTTPSConnection('failteireland.azure-api.net')
-    conn.request("GET", "/opendata-api/v1/attractions?%s" % params, "{body}", headers)
-    response = conn.getresponse()
-    data = response.read()
-    print(data)
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
-
-    
+  
 def routes(request):
+    headers = {
+    }
+
+    params = urllib.parse.urlencode({
+    })
+
+    try:
+        conn = http.client.HTTPSConnection('failteireland.azure-api.net')
+        conn.request("GET", "/opendata-api/v1/attractions?%s" % params, "{body}", headers)
+        response = conn.getresponse()
+        data = response.read()
+        print("This is the print statement: ", data)
+        conn.close()
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
     return render(request, "routes/routes.html")

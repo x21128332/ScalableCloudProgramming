c0bcc7225024751dd9eca3984eef8240b1db5e63
Aisling Kearney <x21128332@student.ncirl.ie>
2023-04-01 18:17:27 +0100

    testing failteirelands api

--- routes/views.py
+++ routes/views.py
@@ -3,8 +3,24 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
+import http.client, urllib.request, urllib.parse, urllib.error, base64
 
+headers = {
+}
+
+params = urllib.parse.urlencode({
+})
+
+try:
+    conn = http.client.HTTPSConnection('failteireland.azure-api.net')
+    conn.request("GET", "/opendata-api/v1/attractions?%s" % params, "{body}", headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+    
 def routes(request):
     return render(request, "routes/routes.html")
-
-
